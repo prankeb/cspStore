91e861ae89237d7d8084d992172263b12f49f190
Brandon Pranke <Prankeb@csp.edu>
2024-02-19 12:22:06 -0600

    connects to database

--- backend/server.js
+++ backend/server.js
@@ -58,9 +58,9 @@ app.post('/register', async (req, res) => {
     }
   });
 
-  app.use(express.static(path.join(__dirname, '..', 'build')));
+  app.use(express.static(path.join(__dirname, '..', 'public')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
   });
   
   app.listen(port, () => {
