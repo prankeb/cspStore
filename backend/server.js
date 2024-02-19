796a8a04e5fd8bd9cddca7a82a5c44229e97a497
Brandon Pranke <Prankeb@csp.edu>
2024-02-19 12:59:32 -0600

    connects to database

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,11 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('../public'));
+  app.use(express.static('../build'));
 
   // Serve the React app
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
   });
 }
 
